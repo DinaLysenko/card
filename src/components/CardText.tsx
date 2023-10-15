d48d300fdc8b5153a94ffc5eacf15c3cbafbac6d
DinaLysenko <dinafate@rambler.ru>
2023-10-15 23:46:38 +0500

    refactor

--- src/components/CardText.tsx
+++ src/components/CardText.tsx
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
 export const CardText=styled.p`
- 
-    width: 260px;
-    height: 40px;
-    color: #ABB3BA;
-    font-size: 12px;
-    margin: 20px;
-    line-height: 20px;
-  
+
+  width: 260px;
+  color: #ABB3BA;
+  font-size: 12px;
+  margin: 20px;
+  line-height: 20px;
+
 `
--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -3,11 +3,10 @@ import {myTheme} from "../styles/Theme.styled";
 
 export const Headline = styled.h3`
 
-    width: 70px;
-    height: 19px;
-    font-family: ${myTheme.font.fontfamily};
-    padding: 0 20px;
-    margin: 0;
-    text-align: left;
-  
+  width: 70px;
+  font-family: ${myTheme.font.fontfamily};
+  padding: 0 20px;
+  margin: 0;
+  text-align: left;
+
 `
